8cb43912fc9e236a757ce8bc281c6194864802d7
amitke841 <amiti.keshet@gmail.com>
2023-02-16 21:19:11 +0200

    added graphics for PC's

--- Paraboulut.py
+++ Paraboulut.py
@@ -1,4 +1,4 @@
-#start
+# start
 
 import numpy as np
 import turtle
@@ -6,71 +6,77 @@ import matplotlib.pyplot as plt
 import colorama
 from colorama import Fore
 
-def quaequ():
-    global x1,x2
-    delta = (b**2-4*a*c)
+def quad_equation(a, b, c):
+    delta = (b ** 2 - 4 * a * c)
+    x1 = (-b + np.sqrt(delta)) / (2 * a)
+    x2 = (-b - np.sqrt(delta)) / (2 * a)
+    return x1, x2
 
-    x1 = (-b+np.sqrt(delta))/(2*a)
-    x2 = (-b-np.sqrt(delta))/(2*a)
-    
-    print(Fore.RED + "Quadratic Equation Results:")
-    print("X1 is ",x1)
-    print("X2 is ",x2)
 
-def Posneg():
-    global mm
+def pos_neg(a):
+    mm = ""
     if a > 0:
-        #postive Number
+        # postive number
         mm = "min"
         print(Fore.YELLOW + "Parabula is Postive")
     elif a == 0:
-        #zero
-        mm = "0"
+        # zero
+        exit("Error: the input A cannot be 0.")
     else:
-        #negative number
+        # negative number
         mm = "max"
         print(Fore.YELLOW + "Parabula is Negative")
+    return mm
 
-def Findkod():
-    global Xkod
-    global Ykod
-    Xkod = (-b)/(2*a)
-    #print("X KodKod is ", Xkod)
-    Ykod = a* Xkod**2 + b*Xkod + c
-    #print("Y KodKod is ", Ykod)
 
-def Tzir():
-    global Xkod
-    print("Tzir X =", Xkod)
+def find_kodkod(a, b, c):
+    x_kod = (-b) / (2 * a)
+    # print("X KodKod is ", Xkod)
+    y_kod = a * x_kod ** 2 + b * x_kod + c
+    # print("Y KodKod is ", Ykod)
+    return x_kod, y_kod
 
-def Nekudot():
-    global Ypoint
-    Ypoint = c
-    quaequ()
-    Points = "(" + str(x1) + ",0), (" + str(x2) + ",0), (0," + str(Ypoint) + ")"
+
+if __name__ == '__main__':
+
+    if 0:
+        # get input from user
+        print(Fore.GREEN + "start")
+        A = int(input(Fore.WHITE + "Input A:"))
+        B = int(input("Input B:"))
+        C = int(input("Input C:"))
+    else:
+        # just for debug
+        A = 1
+        B = -8
+        C = 7
+
+    min_max = pos_neg(A)
+    kodkod = find_kodkod(A, B, C)
+    x1, x2 = quad_equation(A, B, C)
+
+    print(min_max + str(kodkod))
+    (x_kod, y_kod) = kodkod
+    print("Tzir X =", x_kod)
+    Points = "(" + str(x1) + ",0), (" + str(x2) + ",0), (0," + str(C) + ")"
     print(Fore.YELLOW + Points)
+    print(Fore.RED + "Quadratic Equation Results:")
+    print("X1 is ", x1)
+    print("X2 is ", x2)
 
-def Graph():
     plt.style.use('_mpl-gallery')
-    x  = np.linspace(-50,50,100)
-    y = a*(x**2)+b*x+c
-    plt.plot(x,y)
+    x = np.linspace(-2 + min(x1, x2), 2 + max(x1, x2), 100)
+    y_axis = x * 0
+    y = A * (x ** 2) + B * x + C
+    x_axis = y * 0
+    plt.figure(figsize=(10, 6))
+    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+    plt.plot(x, y_axis, color="k")
+    plt.plot(x_axis, y, color="k")
+    plt.plot(x_kod*np.ones(100), y, color="gray", linestyle = "--")
+    plt.plot(x, y, color="r")
+    plt.plot(x1, 0.0, marker="o", markerfacecolor="green", markersize=10, markeredgecolor="red")
+    plt.plot(x2, 0.0, marker="o", markerfacecolor="green", markersize=10, markeredgecolor="red")
+    plt.plot(0, C, marker="o", markerfacecolor="green", markersize=10, markeredgecolor="red")
+    plt.plot(x_kod, y_kod, marker="o", markerfacecolor="blue", markersize=10, markeredgecolor="red")
     plt.show()
-    #plt.axis('equal')
-
-
-print(Fore.GREEN + "start")
-
-
-a = int(input(Fore.WHITE + "Input A:"))
-b = int(input("Input B:"))
-c = int(input("Input C:"))
-
-Posneg()
-Findkod()
-KodKod = Xkod, Ykod
-print(mm + str(KodKod))
-Tzir()
-Nekudot()
-Graph()
-#s = turtle.getscreen()
--- quaequ.py
+++ quaequ.py
@@ -1,6 +1,7 @@
-#start
+# start
 
 import numpy
+
 
 def quaequ():
     print("Input A:")
@@ -10,14 +11,15 @@ def quaequ():
     print("Input C:")
     c = int(input())
 
-    #let Numpy = x1 +- x2
-    delta = (b**2-4*a*c)
+    # let Numpy = x1 +- x2
+    delta = (b ** 2 - 4 * a * c)
 
-    x1 = (-b+numpy.sqrt(delta))/(2*a)
-    x2 = (-b-numpy.sqrt(delta))/(2*a)
+    x1 = (-b + numpy.sqrt(delta)) / (2 * a)
+    x2 = (-b - numpy.sqrt(delta)) / (2 * a)
 
-    print("X1 is ",x1)
-    print("X2 is ",x2)
+    print("X1 is ", x1)
+    print("X2 is ", x2)
+
 
 print("start")
 quaequ()
